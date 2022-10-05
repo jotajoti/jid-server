eb49ca2f38b3a91bf3e035aa7f8c90e0ef86391a
Michael Kringelhede <m@kringelhede.com>
2022-10-05 19:13:56 +0200

    Removed Sentry

--- app/api.js
+++ app/api.js
@@ -3,8 +3,6 @@
 //ExpressJS for rest API
 import express from 'express';
 import cors from 'cors';
-import * as Sentry from '@sentry/node';
-import * as Apm from '@sentry/apm';
 //API
 import * as config from './config.js';
 import * as users from './users.js';
@@ -12,7 +10,7 @@ import * as stats from './stats.js';
 import * as jid from './jid.js';
 
 export async function startServer(args) {
-    if (!args) { 
+    if (!args) {
         args = {}
     }
     const port = args.port || 4000;
@@ -25,20 +23,11 @@ export async function startServer(args) {
     const http = require('http').createServer(app);
     const io = require('socket.io')(http, { origins: '*:*'});
 
-    Sentry.init({
-        dsn: 'https://5810e3ec687d4e3b986eb158a0c24a8b@sentry.billestauner.dk/3',
-        integrations: [
-            // enable HTTP calls tracing
-            new Sentry.Integrations.Http({tracing: true}),
-            // enable Express.js middleware tracing
-            new Apm.Integrations.Express({app})
-        ],
-        tracesSampleRate: 1.0
-    });
-    app.use(Sentry.Handlers.requestHandler());
-    app.use(Sentry.Handlers.tracingHandler());
-
-    app.use(cors());
+    //Only enable CORS if explicitly selected with runtime parameter
+    if (args.cors) {
+        console.log("Enabling Cross-origin resource sharing (CORS) on the server");
+        app.use(cors());
+    }
     app.use(express.json());
     app.use(function (req, res, next) {
         res.locals.db = args.database;
@@ -54,11 +43,7 @@ export async function startServer(args) {
 
     app.get('/stats', stats.getStats);
 
-    app.use(Sentry.Handlers.errorHandler());
-
     http.listen(port, () => {
-        if (config.isLoggingInfo()) {
-            console.log(`Server running on port ${port}!`);
-        }
+        console.log(`Server running on port ${port}!`);
     });
 }
